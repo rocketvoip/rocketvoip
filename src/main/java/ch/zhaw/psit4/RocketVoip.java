78436a1343ddebdacbe0e05cef74791d8186edff
Rafael Ostertag <rafi@guengel.ch>
2017-02-27 22:10:20 +0100

    Removed redundant @EnableAutoConfiguration

--- src/main/java/ch/zhaw/psit4/RocketVoip.java
+++ src/main/java/ch/zhaw/psit4/RocketVoip.java
@@ -1,7 +1,6 @@
 package ch.zhaw.psit4;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -9,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
  * @author Rafael Ostertag
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 @ComponentScan
 public class RocketVoip {
     public static void main(String[] args) throws Exception {
