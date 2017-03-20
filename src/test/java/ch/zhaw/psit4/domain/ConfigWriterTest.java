c4634d8a95d88dc631b5d74cea2f6103cde848a5
braunjon <braunjon@students.zhaw.ch>
2017-03-20 16:53:47 +0100

    Add test for ConfigWriter

---
+++ src/test/java/ch/zhaw/psit4/domain/ConfigWriterTest.java
@@ -0,0 +1,39 @@
+package ch.zhaw.psit4.domain;
+
+import ch.zhaw.psit4.domain.interfaces.SipClientConfigurationInterface;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for ConfigWriter.
+ *
+ * @author Jona Braun
+ */
+public class ConfigWriterTest {
+
+    private ConfigWriter configWriter;
+    @Mock
+    private SipClientConfigurationInterface sipClientConfiguration;
+
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        configWriter = new ConfigWriter(sipClientConfiguration);
+    }
+
+    @Test
+    public void writeConfigurationFile() throws Exception {
+
+        configWriter.generateSipClientConfiguration(null);
+        verify(sipClientConfiguration, times(1)).generateSipClientConfiguration(null);
+
+    }
+
+
+}
