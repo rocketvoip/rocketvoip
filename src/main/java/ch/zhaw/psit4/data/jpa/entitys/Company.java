03d50921e3abf3f56194ad32af9ff0bc52879833
beni <herzobe1@students.zhaw.ch>
2017-03-20 15:34:29 +0100

    3 entitys created: Company, CompanyAdmin, SipClient

---
+++ src/main/java/ch/zhaw/psit4/data/jpa/entitys/Company.java
@@ -0,0 +1,38 @@
+package ch.zhaw.psit4.data.jpa.entitys;
+
+/**
+ * Table for companys
+ * Created by beni on 20.03.17.
+ */
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Company implements Serializable{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    protected Company(){
+
+    }
+
+    public Company(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
