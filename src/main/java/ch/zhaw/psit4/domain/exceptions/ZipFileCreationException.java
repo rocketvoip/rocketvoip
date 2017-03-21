e5063197b946d1cfd0bea02511416e8513e41d27
braunjon <braunjon@students.zhaw.ch>
2017-03-21 21:58:41 +0100

    Clean up - remove classes from different branch - add exception for the zip writer

---
+++ src/main/java/ch/zhaw/psit4/domain/exceptions/ZipFileCreationException.java
@@ -0,0 +1,28 @@
+package ch.zhaw.psit4.domain.exceptions;
+
+/**
+ * It is thrown if the zip file can't be created.
+ *
+ * @author Jona Braun
+ */
+public class ZipFileCreationException extends RuntimeException {
+    public ZipFileCreationException() {
+        super();
+    }
+
+    public ZipFileCreationException(String message) {
+        super(message);
+    }
+
+    public ZipFileCreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ZipFileCreationException(Throwable cause) {
+        super(cause);
+    }
+
+    public ZipFileCreationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
