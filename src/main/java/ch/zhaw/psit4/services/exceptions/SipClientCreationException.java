fdb31019b1370d2bd54fff236bde1e87efb3b2b2
Rafael Ostertag <rafi@guengel.ch>
2017-03-24 17:09:55 +0100

    Dropped unneeded constructors.

--- src/main/java/ch/zhaw/psit4/services/exceptions/SipClientCreationException.java
+++ src/main/java/ch/zhaw/psit4/services/exceptions/SipClientCreationException.java
@@ -4,24 +4,7 @@ package ch.zhaw.psit4.services.exceptions;
  * @author Rafael Ostertag
  */
 public class SipClientCreationException extends RuntimeException {
-    public SipClientCreationException() {
-        super();
-    }
-
-    public SipClientCreationException(String message) {
-        super(message);
-    }
-
     public SipClientCreationException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public SipClientCreationException(Throwable cause) {
-        super(cause);
-    }
-
-    public SipClientCreationException(String message, Throwable cause, boolean enableSuppression, boolean
-            writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
--- src/main/java/ch/zhaw/psit4/services/exceptions/SipClientDeletionException.java
+++ src/main/java/ch/zhaw/psit4/services/exceptions/SipClientDeletionException.java
@@ -4,24 +4,7 @@ package ch.zhaw.psit4.services.exceptions;
  * @author Rafael Ostertag
  */
 public class SipClientDeletionException extends RuntimeException {
-    public SipClientDeletionException() {
-        super();
-    }
-
-    public SipClientDeletionException(String message) {
-        super(message);
-    }
-
     public SipClientDeletionException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public SipClientDeletionException(Throwable cause) {
-        super(cause);
-    }
-
-    public SipClientDeletionException(String message, Throwable cause, boolean enableSuppression, boolean
-            writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
