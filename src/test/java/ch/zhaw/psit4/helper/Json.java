5eb99659b9ef168408431dd24fbfc3e83467ea3e
Rafael Ostertag <rafi@guengel.ch>
2017-03-24 17:28:43 +0100

    Added toObjectTypeSafe().

--- src/test/java/ch/zhaw/psit4/helper/Json.java
+++ src/test/java/ch/zhaw/psit4/helper/Json.java
@@ -2,6 +2,8 @@ package ch.zhaw.psit4.helper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
+
 /**
  * JSON helper for tests.
  *
@@ -14,7 +16,27 @@ final public class Json {
         // intentionally empty
     }
 
-    public static String toJSon(Object object) throws Exception {
+    /**
+     * Serialize a given object to JSON.
+     *
+     * @param object object.
+     * @return JSON String.
+     * @throws Exception
+     */
+    public static String toJson(Object object) throws Exception {
         return objectMapper.writeValueAsString(object);
+    }
+
+    /**
+     * Deserialize a JSON string.
+     *
+     * @param value JSON String
+     * @param klass class type of result.
+     * @param <T>   Type of result
+     * @return Deserialized object.
+     * @throws IOException
+     */
+    public static <T> T toObjectTypeSafe(String value, Class<T> klass) throws IOException {
+        return objectMapper.readValue(value, klass);
     }
 }
