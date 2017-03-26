a0d71b076535ee6d1169b0643a2b031ca39d614c
braunjon <braunjon@students.zhaw.ch>
2017-03-26 20:02:04 +0200

    update exception methods

--- src/main/java/ch/zhaw/psit4/domain/exceptions/ZipFileCreationException.java
+++ src/main/java/ch/zhaw/psit4/domain/exceptions/ZipFileCreationException.java
@@ -7,8 +7,24 @@ package ch.zhaw.psit4.domain.exceptions;
  */
 public class ZipFileCreationException extends RuntimeException {
 
+    public ZipFileCreationException() {
+        super();
+    }
+
+    public ZipFileCreationException(String message) {
+        super(message);
+    }
+
     public ZipFileCreationException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public ZipFileCreationException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ZipFileCreationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 
 }
