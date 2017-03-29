cd69e5837434643bfea8c6a3302ced954e9942f7
Rafael Ostertag <rafi@guengel.ch>
2017-03-29 06:49:21 +0200

    Introduced WebConfiguration.java. Moved CorsFilter bean to WebConfiguration.java.

--- src/main/java/ch/zhaw/psit4/RocketVoip.java
+++ src/main/java/ch/zhaw/psit4/RocketVoip.java
@@ -2,10 +2,6 @@ package ch.zhaw.psit4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 /**
  * @author Rafael Ostertag
@@ -16,19 +12,5 @@ public class RocketVoip {
         SpringApplication.run(RocketVoip.class, args);
     }
 
-    /**
-     * Enables OPTIONS calls.
-     **/
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.addExposedHeader("Authorization");
-        config.addExposedHeader("Content-Type");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+
 }
