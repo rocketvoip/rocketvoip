70a9550cfe2896091dfa75047059138afbd13842
Rafael Ostertag <rafi@guengel.ch>
2017-04-09 15:02:32 +0200

    Use fixture AdminEntity.

--- src/test/java/ch/zhaw/psit4/security/auxiliary/UserDetailsServiceImplTest.java
+++ src/test/java/ch/zhaw/psit4/security/auxiliary/UserDetailsServiceImplTest.java
@@ -2,6 +2,7 @@ package ch.zhaw.psit4.security.auxiliary;
 
 import ch.zhaw.psit4.data.jpa.entities.Admin;
 import ch.zhaw.psit4.data.jpa.repositories.AdminRepository;
+import ch.zhaw.psit4.fixtures.database.AdminEntity;
 import ch.zhaw.psit4.helper.AdminUserFixture;
 import ch.zhaw.psit4.helper.mocks.AdminRepositoryMock;
 import org.junit.Before;
@@ -24,7 +25,7 @@ public class UserDetailsServiceImplTest {
 
     @Before
     public void setUp() throws Exception {
-        admin = AdminUserFixture.createAdminEntityFixture();
+        admin = AdminEntity.createAdmin(1);
         adminRepositoryMock = AdminRepositoryMock.createAdminRepositoryMock(admin);
         userDetails = new UserDetailsServiceImpl(adminRepositoryMock);
     }
