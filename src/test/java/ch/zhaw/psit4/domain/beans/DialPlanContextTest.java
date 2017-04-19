a95f3b699730cc9e6f7891297d9aee237479f315
Rafael Ostertag <rafi@guengel.ch>
2017-04-19 18:00:50 +0200

    Added DialPlanContextTest.

---
+++ src/test/java/ch/zhaw/psit4/domain/beans/DialPlanContextTest.java
@@ -0,0 +1,33 @@
+package ch.zhaw.psit4.domain.beans;
+
+import org.junit.Test;
+
+/**
+ * @author Rafael Ostertag
+ */
+public class DialPlanContextTest {
+    @Test
+    public void getContextName() throws Exception {
+    }
+
+    @Test
+    public void setContextName() throws Exception {
+    }
+
+    @Test
+    public void getDialPlanExtensionList() throws Exception {
+    }
+
+    @Test
+    public void setDialPlanExtensionList() throws Exception {
+    }
+
+    @Test
+    public void toDialPlanContextConfiguration() throws Exception {
+    }
+
+    @Test
+    public void validate() throws Exception {
+    }
+
+}
