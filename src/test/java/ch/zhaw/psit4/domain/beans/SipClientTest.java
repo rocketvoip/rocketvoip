b35369a973a26b7b430b6766f495039192408eab
Rafael Ostertag <rafi@guengel.ch>
2017-04-19 17:21:36 +0200

    Added SipClientTest

--- src/test/java/ch/zhaw/psit4/domain/beans/SipClientTest.java
+++ src/test/java/ch/zhaw/psit4/domain/beans/SipClientTest.java
@@ -1,5 +1,7 @@
 package ch.zhaw.psit4.domain.beans;
 
+import ch.zhaw.psit4.domain.exceptions.ValidationException;
+import ch.zhaw.psit4.testsupport.convenience.InputStreamStringyfier;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,9 +20,140 @@ public class SipClientTest {
         sipClient = new SipClient();
     }
 
+    @Test
+    public void getUsername() throws Exception {
+        sipClient.setUsername("username");
+        assertThat(sipClient.getUsername(), equalTo("username"));
+    }
+
+    @Test
+    public void getSecret() throws Exception {
+        sipClient.setSecret("secret");
+        assertThat(sipClient.getSecret(), equalTo("secret"));
+    }
+
+    @Test
+    public void setCompany() throws Exception {
+        sipClient.setCompany("with space");
+        assertThat(sipClient.getCompany(), equalTo("with-space"));
+    }
+
+    @Test
+    public void getPhoneNumber() throws Exception {
+        sipClient.setPhoneNumber("phone");
+        assertThat(sipClient.getPhoneNumber(), equalTo("phone"));
+    }
+
+
+    @Test(expected = ValidationException.class)
+    public void nullCompany() throws Exception {
+        sipClient.setCompany(null);
+        sipClient.setPhoneNumber("a");
+        sipClient.setUsername("b");
+        sipClient.setSecret("c");
+
+        sipClient.validate();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void emptyCompany() throws Exception {
+        sipClient.setCompany("");
+        sipClient.setPhoneNumber("a");
+        sipClient.setUsername("b");
+        sipClient.setSecret("c");
+
+        sipClient.validate();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void nullPhoneNumber() throws Exception {
+        sipClient.setCompany("company");
+        sipClient.setPhoneNumber(null);
+        sipClient.setUsername("b");
+        sipClient.setSecret("c");
+
+        sipClient.validate();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void emptyPhoneNumber() throws Exception {
+        sipClient.setCompany("company");
+        sipClient.setPhoneNumber("");
+        sipClient.setUsername("b");
+        sipClient.setSecret("c");
+
+        sipClient.validate();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void nullUsername() throws Exception {
+        sipClient.setCompany("company");
+        sipClient.setPhoneNumber("phone");
+        sipClient.setUsername(null);
+        sipClient.setSecret("c");
+
+        sipClient.validate();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void emptyUsername() throws Exception {
+        sipClient.setCompany("company");
+        sipClient.setPhoneNumber("phone");
+        sipClient.setUsername("");
+        sipClient.setSecret("c");
+
+        sipClient.validate();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void nullSecret() throws Exception {
+        sipClient.setCompany("company");
+        sipClient.setPhoneNumber("phone");
+        sipClient.setUsername("username");
+        sipClient.setSecret(null);
+
+        sipClient.validate();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void emptySecret() throws Exception {
+        sipClient.setCompany("company");
+        sipClient.setPhoneNumber("phone");
+        sipClient.setUsername("username");
+        sipClient.setSecret("");
+
+        sipClient.validate();
+    }
+
+    @Test
+    public void validSipClient() throws Exception {
+        sipClient.setCompany("company");
+        sipClient.setPhoneNumber("phone");
+        sipClient.setUsername("username");
+        sipClient.setSecret("secret");
+
+        sipClient.validate();
+    }
+
+    @Test
+    public void testToSipClientConfiguration() throws Exception {
+        sipClient.setCompany("ACME");
+        sipClient.setPhoneNumber("00001");
+        sipClient.setSecret("secret");
+        sipClient.setUsername("username");
+
+        String expected = InputStreamStringyfier.slurpStream(
+                SipClientTest.class.getResourceAsStream("/fixtures/sipClientTestFixture.txt")
+        );
+
+        assertThat(sipClient.toSipClientConfiguration(), equalTo(expected));
+    }
+
+
+
     // This test is simply done to improve coverage
     @Test
-    public void setId() throws Exception {
+    public void getId() throws Exception {
         assertThat(sipClient.getId(), equalTo(0L));
 
         sipClient.setId(1);
