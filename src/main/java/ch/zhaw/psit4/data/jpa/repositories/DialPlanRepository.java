8f3164738948627f275eba113ce5cdf988db290b
braunjon <braunjon@students.zhaw.ch>
2017-04-19 19:24:14 +0200

    Added DialPlanRepository

---
+++ src/main/java/ch/zhaw/psit4/data/jpa/repositories/DialPlanRepository.java
@@ -0,0 +1,14 @@
+package ch.zhaw.psit4.data.jpa.repositories;
+
+import ch.zhaw.psit4.data.jpa.entities.Company;
+import ch.zhaw.psit4.data.jpa.entities.DialPlan;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * @author Jona Braun
+ */
+public interface DialPlanRepository extends CrudRepository<DialPlan, Long> {
+    List<DialPlan> findByCompany(Company company);
+}
