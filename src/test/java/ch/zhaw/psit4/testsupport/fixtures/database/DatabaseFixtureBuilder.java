5c7c3dbd933079cb1f0f8fc93043c0316c1c090b
Rafael Ostertag <rafi@guengel.ch>
2017-04-21 17:28:54 +0200

    Added support for DialPlan entity fixtures.

--- src/test/java/ch/zhaw/psit4/testsupport/fixtures/database/DatabaseFixtureBuilder.java
+++ src/test/java/ch/zhaw/psit4/testsupport/fixtures/database/DatabaseFixtureBuilder.java
@@ -2,9 +2,11 @@ package ch.zhaw.psit4.testsupport.fixtures.database;
 
 import ch.zhaw.psit4.data.jpa.entities.Admin;
 import ch.zhaw.psit4.data.jpa.entities.Company;
+import ch.zhaw.psit4.data.jpa.entities.DialPlan;
 import ch.zhaw.psit4.data.jpa.entities.SipClient;
 import ch.zhaw.psit4.data.jpa.repositories.AdminRepository;
 import ch.zhaw.psit4.data.jpa.repositories.CompanyRepository;
+import ch.zhaw.psit4.data.jpa.repositories.DialPlanRepository;
 import ch.zhaw.psit4.data.jpa.repositories.SipClientRepository;
 
 import java.util.ArrayList;
@@ -21,20 +23,31 @@ public class DatabaseFixtureBuilder {
     private final SipClientRepository sipClientRepository;
     private final AdminRepository adminRepository;
     private final CompanyRepository companyRepository;
+    private final DialPlanRepository dialPlanRepository;
     private Collection<Company> company;
     private Map<Integer, Admin> adminList;
     private Map<Integer, SipClient> sipClientList;
+    private Map<Integer, DialPlan> dialPlanList;
 
     public DatabaseFixtureBuilder(CompanyRepository companyRepository, AdminRepository adminRepository,
-                                  SipClientRepository
-            sipClientRepository) {
+                                  SipClientRepository sipClientRepository, DialPlanRepository dialPlanRepository) {
         this.companyRepository = companyRepository;
         this.adminRepository = adminRepository;
         this.sipClientRepository = sipClientRepository;
+        this.dialPlanRepository = dialPlanRepository;
 
         this.company = new ArrayList<>();
         this.adminList = new HashMap<>();
         this.sipClientList = new HashMap<>();
+        this.dialPlanList = new HashMap<>();
+    }
+
+    public DialPlanRepository getDialPlanRepository() {
+        return dialPlanRepository;
+    }
+
+    public Map<Integer, DialPlan> getDialPlanList() {
+        return dialPlanList;
     }
 
     public SipClientRepository getSipClientRepository() {
@@ -75,6 +88,11 @@ public class DatabaseFixtureBuilder {
         return this;
     }
 
+    public DatabaseFixtureBuilder addDialPlan(int number) {
+        dialPlanList.put(number, DialPlanEntity.createDialPlanEntity(number));
+        return this;
+    }
+
     public void build() {
         companyRepository.save(company);
 
@@ -91,6 +109,13 @@ public class DatabaseFixtureBuilder {
                     )
             );
             sipClientRepository.save(x);
+        });
+
+        dialPlanList.values().forEach(x -> {
+            x.setCompany(company.stream().findFirst().orElseThrow(
+                    () -> new RuntimeException("No companies in builder")
+            ));
+            dialPlanRepository.save(x);
         });
     }
 
