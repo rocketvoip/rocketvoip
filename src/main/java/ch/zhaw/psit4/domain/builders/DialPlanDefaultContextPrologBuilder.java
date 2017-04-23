12ba5d7c8f87dc089cb681c11f72c6fd7aaf995b
Rafael Ostertag <rafi@guengel.ch>
2017-04-23 11:01:19 +0200

    Added DialPlanDefaultContextPrologBuilder.java.

--- src/main/java/ch/zhaw/psit4/domain/builders/DialPlanDefaultContextPrologBuilder.java
+++ src/main/java/ch/zhaw/psit4/domain/builders/DialPlanDefaultContextPrologBuilder.java
@@ -1,20 +1,26 @@
 package ch.zhaw.psit4.domain.builders;
 
-import ch.zhaw.psit4.domain.beans.DialPlanContext;
+import ch.zhaw.psit4.domain.beans.DialPlanExtension;
+import ch.zhaw.psit4.domain.dialplan.applications.RingingApp;
+import ch.zhaw.psit4.domain.dialplan.applications.WaitApp;
 
 /**
- * Build context with a default prolog. A default prolog is
+ * Build context with a default prolog. A default prolog is added
  * <code>
  * exten => x,1, Ringing
  * exten => x,2, Wait(n)
  * </code>
  * <p>
- * When using DialPlanDefaultContextPrologBuilder#addNewContext(), {$code n} is 2.
+ *
+ * The default of {@code n} is 2. It can be changed by calling
+ * DialPlanDefaultContextPrologBuilder#setWaitInSeconds
  *
  * @author Rafael Ostertag
  */
 public class DialPlanDefaultContextPrologBuilder extends DialPlanConfigBuilder {
-    public static final int DEFAULT_WAIT = 2;
+    public static final String RINGING_PRIORITY = "1";
+    public static final String WAIT_PRIORITY = "2";
+    private int waitInSeconds = 2;
 
     public DialPlanDefaultContextPrologBuilder() {
         super();
@@ -24,23 +30,57 @@ public class DialPlanDefaultContextPrologBuilder extends DialPlanConfigBuilder {
         super(dialPlanConfigBuilder);
     }
 
-    /**
-     * Add a new context and add the default prolog. The value to Asterisk {$code Wait} is 2.
-     *
-     * @param context the new dialplan context.
-     * @return DialPlanDefaultContextPrologBuilder instance.
-     */
-    @Override
-    public DialPlanDefaultContextPrologBuilder addNewContext(DialPlanContext context) {
-        return this.addNewContext(context, DEFAULT_WAIT);
+    public int getWaitInSeconds() {
+        return waitInSeconds;
     }
 
-    public DialPlanDefaultContextPrologBuilder addNewContext(DialPlanContext context, int waitInSeconds) {
-        super.addNewContext(context);
-        //DialPlanContext dialPlanContext = getActiveContext();
+    /**
+     * Set the wait before answering in seconds. This value is valid only for newly added contexts. Existing contexts
+     * won't have its wait seconds changed.
+     *
+     * @param waitInSeconds new wait value
+     */
+    public DialPlanDefaultContextPrologBuilder setWaitInSeconds(int waitInSeconds) {
+        this.waitInSeconds = waitInSeconds;
+        return this;
+    }
 
-        //dialPlanContext.getDialPlanExtensionList()
+    @Override
+    public DialPlanDefaultContextPrologBuilder addNewExtension(DialPlanExtension extension) {
+        super.addNewExtension(extension);
+
+        // We need an active extension in oder to figure out what the phone number is.
+        DialPlanExtension activeDialPlanExtension = getActiveExtension();
+
+        // Now, we know the phone number, so we create the first entry of our prolog
+        DialPlanExtension ringingExtension = makeRingingExtension(activeDialPlanExtension.getPhoneNumber());
+        // and add it to the front of the active context
+        getActiveContext().getDialPlanExtensionList().add(0, ringingExtension);
+        // we need to add the wait call, to complete our prolog
+        DialPlanExtension waitExtension = makeWaitExtension(activeDialPlanExtension.getPhoneNumber());
+        getActiveContext().getDialPlanExtensionList().add(1, waitExtension);
 
         return this;
+    }
+
+    private DialPlanExtension makeWaitExtension(String phoneNumber) {
+        DialPlanExtension waitExtension = new DialPlanExtension();
+        waitExtension.setPriority(WAIT_PRIORITY);
+        waitExtension.setPhoneNumber(phoneNumber);
+
+        waitExtension.setDialPlanApplication(new WaitApp(waitInSeconds));
+
+        return waitExtension;
+
+    }
+
+    private DialPlanExtension makeRingingExtension(String phoneNumber) {
+        DialPlanExtension ringingExtension = new DialPlanExtension();
+        ringingExtension.setPriority(RINGING_PRIORITY);
+        ringingExtension.setPhoneNumber(phoneNumber);
+        ringingExtension.setDialPlanApplication(new RingingApp());
+
+        return ringingExtension;
+
     }
 }
---
+++ src/test/java/ch/zhaw/psit4/domain/builders/DialPlanDefaultContextPrologBuilderTest.java
@@ -0,0 +1,79 @@
+package ch.zhaw.psit4.domain.builders;
+
+import ch.zhaw.psit4.domain.beans.DialPlanContext;
+import ch.zhaw.psit4.domain.beans.DialPlanExtension;
+import ch.zhaw.psit4.testsupport.convenience.InputStreamStringyfier;
+import ch.zhaw.psit4.testsupport.fixtures.domain.DialPlanContextGenerator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Rafael Ostertag
+ */
+public class DialPlanDefaultContextPrologBuilderTest {
+    private DialPlanDefaultContextPrologBuilder dialPlanDefaultContextPrologBuilder;
+
+    @Before
+    public void setUp() throws Exception {
+        dialPlanDefaultContextPrologBuilder = new DialPlanDefaultContextPrologBuilder();
+    }
+
+    @Test
+    public void setWaitInSeconds() throws Exception {
+        DialPlanContext context = DialPlanContextGenerator.dialPlanContext(1);
+
+        dialPlanDefaultContextPrologBuilder
+                .setWaitInSeconds(4)
+                .addNewContext(context);
+
+        DialPlanExtension dialPlanExtension = makeMockDialPlan();
+
+        dialPlanDefaultContextPrologBuilder.addNewExtension(dialPlanExtension);
+        String expected = InputStreamStringyfier.slurpStream(
+                DialPlanDefaultContextPrologBuilderTest.class.getResourceAsStream
+                        ("/fixtures/dialPlanDefaultContextPrologBuilderWait4Fixture.txt")
+        );
+
+        List<DialPlanContext> contexts = dialPlanDefaultContextPrologBuilder.build();
+        assertThat(contexts, hasSize(1));
+
+        assertThat(contexts.get(0).toDialPlanContextConfiguration(), equalTo(expected));
+    }
+
+    @Test
+    public void addNewExtension() throws Exception {
+        DialPlanContext context = DialPlanContextGenerator.dialPlanContext(1);
+
+        dialPlanDefaultContextPrologBuilder.addNewContext(context);
+        DialPlanExtension dialPlanExtension = makeMockDialPlan();
+
+        dialPlanDefaultContextPrologBuilder.addNewExtension(dialPlanExtension);
+        String expected = InputStreamStringyfier.slurpStream(
+                DialPlanDefaultContextPrologBuilderTest.class.getResourceAsStream
+                        ("/fixtures/dialPlanDefaultContextPrologBuilderFixture.txt")
+        );
+
+        List<DialPlanContext> contexts = dialPlanDefaultContextPrologBuilder.build();
+        assertThat(contexts, hasSize(1));
+
+        assertThat(contexts.get(0).toDialPlanContextConfiguration(), equalTo(expected));
+
+    }
+
+    public DialPlanExtension makeMockDialPlan() {
+        DialPlanExtension dialPlanExtension = mock(DialPlanExtension.class);
+        when(dialPlanExtension.getPhoneNumber()).thenReturn("0001");
+        when(dialPlanExtension.getPriority()).thenReturn("10");
+        when(dialPlanExtension.toDialPlanExtensionConfiguration()).thenReturn("exten=> 0001, 10, mockExtension\n");
+        return dialPlanExtension;
+    }
+
+}
