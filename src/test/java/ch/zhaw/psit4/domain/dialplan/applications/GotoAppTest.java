83355cc4cbb754bdb4974a1159b595f74ff2f7c2
Rafael Ostertag <rafi@guengel.ch>
2017-04-23 17:51:10 +0200

    Added GotoApp.

---
+++ src/test/java/ch/zhaw/psit4/domain/dialplan/applications/GotoAppTest.java
@@ -0,0 +1,37 @@
+package ch.zhaw.psit4.domain.dialplan.applications;
+
+import ch.zhaw.psit4.domain.exceptions.ValidationException;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Rafael Ostertag
+ */
+public class GotoAppTest {
+    @Test(expected = ValidationException.class)
+    public void validateNullReference() throws Exception {
+        GotoApp gotoApp = new GotoApp(null);
+        gotoApp.validate();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void validateEmptyReference() throws Exception {
+        GotoApp gotoApp = new GotoApp("");
+        gotoApp.validate();
+    }
+
+    @Test
+    public void validate() throws Exception {
+        GotoApp gotoApp = new GotoApp("reference");
+        gotoApp.validate();
+    }
+
+    @Test
+    public void toApplicationCall() throws Exception {
+        GotoApp gotoApp = new GotoApp("reference");
+        assertThat(gotoApp.toApplicationCall(), equalTo("Goto(reference)"));
+    }
+
+}
