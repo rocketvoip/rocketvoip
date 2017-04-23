5b7b7f2c22dd53a0c007847d8d8cbd131d7b114e
Rafael Ostertag <rafi@guengel.ch>
2017-04-23 15:56:00 +0200

    Extended tests.

--- src/test/java/ch/zhaw/psit4/domain/builders/DialPlanDefaultContextPrologBuilderTest.java
+++ src/test/java/ch/zhaw/psit4/domain/builders/DialPlanDefaultContextPrologBuilderTest.java
@@ -68,11 +68,60 @@ public class DialPlanDefaultContextPrologBuilderTest {
 
     }
 
-    public DialPlanExtension makeMockDialPlan() {
+    @Test
+    public void addNewTwoExtension() throws Exception {
+        DialPlanContext context = DialPlanContextGenerator.dialPlanContext(1);
+
+        dialPlanDefaultContextPrologBuilder.addNewContext(context);
+        DialPlanExtension dialPlanExtension = makeMockDialPlan();
+
+        dialPlanDefaultContextPrologBuilder.addNewExtension(dialPlanExtension);
+        dialPlanDefaultContextPrologBuilder.addNewExtension(dialPlanExtension);
+        String expected = InputStreamStringyfier.slurpStream(
+                DialPlanDefaultContextPrologBuilderTest.class.getResourceAsStream
+                        ("/fixtures/dialPlanDefaultContextPrologBuilderTwoExtensionFixture.txt")
+        );
+
+        List<DialPlanContext> contexts = dialPlanDefaultContextPrologBuilder.build();
+        assertThat(contexts, hasSize(1));
+
+        assertThat(contexts.get(0).toDialPlanContextConfiguration(), equalTo(expected));
+
+    }
+
+    @Test
+    public void addNewTwoExtensionWithReversePriority() throws Exception {
+        DialPlanContext context = DialPlanContextGenerator.dialPlanContext(1);
+
+        dialPlanDefaultContextPrologBuilder.addNewContext(context);
+        DialPlanExtension dialPlanExtension = makeMockDialPlan("20");
+
+        dialPlanDefaultContextPrologBuilder.addNewExtension(dialPlanExtension);
+
+        dialPlanExtension = makeMockDialPlan("10");
+        dialPlanDefaultContextPrologBuilder.addNewExtension(dialPlanExtension);
+        String expected = InputStreamStringyfier.slurpStream(
+                DialPlanDefaultContextPrologBuilderTest.class.getResourceAsStream
+                        ("/fixtures/dialPlanDefaultContextPrologBuilderTwoExtensionReversePriorityFixture.txt")
+        );
+
+        List<DialPlanContext> contexts = dialPlanDefaultContextPrologBuilder.build();
+        assertThat(contexts, hasSize(1));
+
+        assertThat(contexts.get(0).toDialPlanContextConfiguration(), equalTo(expected));
+
+    }
+
+    private DialPlanExtension makeMockDialPlan() {
+        return makeMockDialPlan("10");
+    }
+
+    private DialPlanExtension makeMockDialPlan(String priority) {
         DialPlanExtension dialPlanExtension = mock(DialPlanExtension.class);
         when(dialPlanExtension.getPhoneNumber()).thenReturn("0001");
-        when(dialPlanExtension.getPriority()).thenReturn("10");
-        when(dialPlanExtension.toDialPlanExtensionConfiguration()).thenReturn("exten=> 0001, 10, mockExtension\n");
+        when(dialPlanExtension.getPriority()).thenReturn(priority);
+        when(dialPlanExtension.toDialPlanExtensionConfiguration()).thenReturn("exten=> 0001, " + priority + ", " +
+                "mockExtension\n");
         return dialPlanExtension;
     }
 
