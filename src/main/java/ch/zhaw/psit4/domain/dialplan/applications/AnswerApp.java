c34530fa3aa8a50ff46b44e294f3d9ac5fe384c7
Rafael Ostertag <rafi@guengel.ch>
2017-05-01 17:12:31 +0200

    Added AnswerApp

---
+++ src/main/java/ch/zhaw/psit4/domain/dialplan/applications/AnswerApp.java
@@ -0,0 +1,23 @@
+package ch.zhaw.psit4.domain.dialplan.applications;
+
+import ch.zhaw.psit4.domain.interfaces.DialPlanAppInterface;
+
+/**
+ * @author Rafael Ostertag
+ */
+public class AnswerApp implements DialPlanAppInterface {
+    @Override
+    public void validate() {
+        // intentionally empty
+    }
+
+    @Override
+    public String toApplicationCall() {
+        return "Answer";
+    }
+
+    @Override
+    public boolean requireAnswer() {
+        return false;
+    }
+}
---
+++ src/test/java/ch/zhaw/psit4/domain/dialplan/applications/AnswerAppTest.java
@@ -0,0 +1,35 @@
+package ch.zhaw.psit4.domain.dialplan.applications;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Rafael Ostertag
+ */
+public class AnswerAppTest {
+    private AnswerApp answerApp;
+
+    @Before
+    public void setUp() throws Exception {
+        answerApp = new AnswerApp();
+    }
+
+    @Test
+    public void validate() throws Exception {
+        answerApp.validate();
+    }
+
+    @Test
+    public void toApplicationCall() throws Exception {
+        assertThat(answerApp.toApplicationCall(), equalTo("Answer"));
+    }
+
+    @Test
+    public void requireAnswer() throws Exception {
+        assertThat(answerApp.requireAnswer(), equalTo(false));
+    }
+
+}
