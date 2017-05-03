e1665778a2114790718579010d60eb9a459a9676
Rafael Ostertag <rafi@guengel.ch>
2017-05-03 17:34:32 +0200

    Dropped unused c'tors from Exceptions.

--- src/main/java/ch/zhaw/psit4/domain/exceptions/InvalidConfigurationException.java
+++ src/main/java/ch/zhaw/psit4/domain/exceptions/InvalidConfigurationException.java
@@ -7,24 +7,7 @@ package ch.zhaw.psit4.domain.exceptions;
  */
 public class InvalidConfigurationException extends RuntimeException {
 
-    public InvalidConfigurationException() {
-        super();
-    }
-
     public InvalidConfigurationException(String message) {
         super(message);
     }
-
-    public InvalidConfigurationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidConfigurationException(Throwable cause) {
-        super(cause);
-    }
-
-    protected InvalidConfigurationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
 }
