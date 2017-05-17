5a77d7005979efef1175ee1b2237bccdf8edd226
Rafael Ostertag <rafi@guengel.ch>
2017-05-17 06:32:01 +0200

    Added `getAllDialPlansForCompanies()`.

--- src/main/java/ch/zhaw/psit4/services/interfaces/DialPlanServiceInterface.java
+++ src/main/java/ch/zhaw/psit4/services/interfaces/DialPlanServiceInterface.java
@@ -1,3 +1,32 @@
+/*
+ * Copyright 2017 Jona Braun, Benedikt Herzog, Rafael Ostertag,
+ *                Marcel Schöni, Marco Studerus, Martin Wittwer
+ *
+ * Redistribution and  use in  source and binary  forms, with  or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions  of  source code  must retain  the above  copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in  binary form must reproduce  the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation   and/or   other    materials   provided   with   the
+ *    distribution.
+ *
+ * THIS SOFTWARE  IS PROVIDED BY  THE COPYRIGHT HOLDERS  AND CONTRIBUTORS
+ * "AS  IS" AND  ANY EXPRESS  OR IMPLIED  WARRANTIES, INCLUDING,  BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE  ARE DISCLAIMED. IN NO EVENT  SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL DAMAGES  (INCLUDING,  BUT  NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS  INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF  LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY,  OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package ch.zhaw.psit4.services.interfaces;
 
 import ch.zhaw.psit4.dto.DialPlanDto;
@@ -21,6 +50,15 @@ public interface DialPlanServiceInterface {
      * @throws DialPlanRetrievalException Implementations are expected to throw DialPlanRetrievalExcpetion on error.
      */
     List<DialPlanDto> getAllDialPlans();
+
+    /**
+     * Retrieve all DialPlans for given companies.
+     *
+     * @param companyIds list of company IDs
+     * @return list of all DialPlans, or an empty list if no DialPlans are found
+     * @throws DialPlanRetrievalException Implementations are expected to throw DialPlanRetrievalExcpetion on error.
+     */
+    List<DialPlanDto> getAllDialPlansForCompanies(List<Long> companyIds);
 
     /**
      * Create a new DialPlan. The {$code id} attribute of {$code newDialPlan} will be ignored if set. The returned
