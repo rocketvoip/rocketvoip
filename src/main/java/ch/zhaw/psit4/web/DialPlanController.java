85a7224e972a39e0b92e587f64fc65e6e213d3bc
Rafael Ostertag <rafi@guengel.ch>
2017-05-17 06:17:47 +0200

    Simplified `DialPlanController`.

--- src/main/java/ch/zhaw/psit4/web/DialPlanController.java
+++ src/main/java/ch/zhaw/psit4/web/DialPlanController.java
@@ -1,3 +1,32 @@
+/*
+ * Copyright 2017 Jona Braun, Benedikt Herzog, Rafael Ostertag,
+ *                Marcel Schöni, Marco Studerus, Martin Wittwer
+ *
+ * Redistribution and  use in  source and binary  forms, with  or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions  of  source code  must retain  the above  copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in  binary form must reproduce  the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation   and/or   other    materials   provided   with   the
+ *    distribution.
+ *
+ * THIS SOFTWARE  IS PROVIDED BY  THE COPYRIGHT HOLDERS  AND CONTRIBUTORS
+ * "AS  IS" AND  ANY EXPRESS  OR IMPLIED  WARRANTIES, INCLUDING,  BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE  ARE DISCLAIMED. IN NO EVENT  SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL DAMAGES  (INCLUDING,  BUT  NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS  INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF  LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY,  OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package ch.zhaw.psit4.web;
 
 import ch.zhaw.psit4.dto.DialPlanDto;
@@ -15,7 +44,7 @@ import java.util.List;
  * @author Jona Braun
  */
 @RestController
-@RequestMapping(path = "/v1", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@RequestMapping(path = "/v1/dialplans", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class DialPlanController {
     private final DialPlanServiceInterface dialPlanServiceInterface;
 
@@ -23,29 +52,29 @@ public class DialPlanController {
         this.dialPlanServiceInterface = dialPlanServiceInterface;
     }
 
-    @GetMapping(path = "/dialplans")
+    @GetMapping
     public ResponseEntity<List<DialPlanDto>> getAllDialPlans() {
         return new ResponseEntity<>(dialPlanServiceInterface.getAllDialPlans(), HttpStatus.OK);
     }
 
-    @GetMapping(path = "/dialplans/{id}")
+    @GetMapping(path = "/{id}")
     public ResponseEntity<DialPlanDto> getDialPlan(@PathVariable long id) {
         return new ResponseEntity<>(dialPlanServiceInterface.getDialPlan(id), HttpStatus.OK);
     }
 
-    @DeleteMapping(path = "/dialplans/{id}")
+    @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> deleteDialPlan(@PathVariable long id) {
         dialPlanServiceInterface.deleteDialPlan(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping(path = "/dialplans/{id}")
+    @PutMapping(path = "/{id}")
     public ResponseEntity<DialPlanDto> updateDialPlan(@PathVariable long id, @RequestBody DialPlanDto dialPlanDto) {
         dialPlanDto.setId(id);
         return new ResponseEntity<>(dialPlanServiceInterface.updateDialPlan(dialPlanDto), HttpStatus.OK);
     }
 
-    @PostMapping(path = "/dialplans")
+    @PostMapping
     public ResponseEntity<DialPlanDto> createDialPlan(@RequestBody DialPlanDto dialPlanDto) {
         return new ResponseEntity<>(dialPlanServiceInterface.createDialPlan(dialPlanDto), HttpStatus.CREATED);
     }
