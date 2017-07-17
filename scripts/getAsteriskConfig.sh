44ee390582a9acddfe934cc4500fc44e078fbc83
mawit <mawit@users.noreply.github.com>
2017-07-17 11:25:41 +0200

    Update getAsteriskConfig.sh

--- scripts/getAsteriskConfig.sh
+++ scripts/getAsteriskConfig.sh
@@ -12,15 +12,17 @@
 # Vars
 ASTERISK_CONFIG_PATH="/opt/asterisk/etc/asterisk"
 DOWNLOAD_FOLDER="/opt/asterisk/etc/asterisk/downloads"
-API_PATH="https://rocketvoip.herokuapp.com/v1/configuration/zip"
+API_BASE_URL="https://rocketvoip-staging.herokuapp.com/v1/"
+API_PATH="${API_BASE_URL}configuration/zip"
+API_USER="masteradmin@rocketvoip.local"
+API_PASS="4h6rb42q7eoupbn185a4vodeot"
 DATE=$(date +%Y%m%d)
 EXPIRE_DAYS="10"
 
 # Code
 
 # Get Auth Token
-AUTH="$(/usr/bin/curl rocketvoip.herokuapp.com/v1/login -XPOST -d '{"username": "masteradmin@rocketvoip.local", "password": "********"}' -i -H "Content-Type: application/json;charset=UTF-8" | grep X-Auth-Token | sed 's/\r//' )"
-
+AUTH=$(/usr/bin/curl ${API_BASE_URL}login -XPOST -d "{\"username\": \"${API_USER}\", \"password\": \"${API_PASS}\"}" -i -H "Content-Type: application/json;charset=UTF-8" | grep X-Auth-Token | sed 's/\r//')
 # Download config
 /usr/bin/curl -H "${AUTH}" ${API_PATH} -o "${DOWNLOAD_FOLDER}/$DATE.zip"
 if [ "$?" -ne "0" ]; then
